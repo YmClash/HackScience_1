3ace8d42fe46d2384548eab0dcb191daa73de845
YmClash <momo.toure27@gmail.com>
2023-05-25 00:29:52 +0200

    add some New Neuronal Network and  update to the  folder LAB_6 add Choppin's Nocturne 27 2 in  D  flat midi file to Train and test LSTM Network in folder LAB_1

--- LAB_6/akili.py
+++ LAB_6/akili.py
@@ -13,15 +13,16 @@ class LSTMModel(nn.Module):
 
         self.lstm = nn.LSTM(input_size,hidden_size,num_layers,batch_first=True) #la couche LSTM avec les paramètres spécifiés.
         self.fc = nn.Linear(hidden_size,output_size) # la couche entièrement connectée (linéaire) qui prend les états cachés de la dernière cellule LSTM et les convertit en sortie de taille output_size.
-python
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0),self.hidden_size)  # etat  cachè   a  0
-        c0 = torch.zeros(self.num_layers,x.size(0)),self.hidden_size)   # etat celulle  a  0
+        h0 = torch.zeros(self.num_layers, x.size(0),self.hidden_size)  # etat  cachè(hidden)   a  0
+        c0 = torch.zeros(self.num_layers,x.size(0),self.hidden_size)   # etat celulle  a  0
 
 
         out, _ = self.lstm
         out = self.fc(out[])
+
+    return out
 
 
 
